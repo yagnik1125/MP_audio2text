7c551fbae18e6111221cd230dfd9da9798ecab09
yagnik1125 <140049645+yagnik1125@users.noreply.github.com>
2024-09-15 16:29:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 import streamlit as st
-import os
-import numpy as np
-
-try:
-    import tensorflow  # required in Colab to avoid protobuf compatibility issues
-except ImportError:
-    pass
-
-import torch
-import pandas as pd
-import whisper
-import torchaudio
-
-from tqdm.notebook import tqdm
 
 from pydub import AudioSegment
+import googletrans
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 import googletrans
 
